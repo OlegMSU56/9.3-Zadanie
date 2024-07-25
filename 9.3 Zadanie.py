88baca96c8e69ddd78d0916effc962f3a69026d7
Oleg <sagdeev.ol@yandex.ru>
2024-07-25 22:53:22 +0300

    Update 9.3 Zadanie.py

--- 9.3 Zadanie.py
+++ 9.3 Zadanie.py
@@ -1,12 +1,13 @@
 def all_variants(text):
-    for i in text:
-        yield i
-    b = len(text)
-    for first in range(b):
-        for last in range(first + 1, b+1):
-            if last - first > 1:
-                yield text[first:last]
-
+    length = len(text)
+    # Генерируем одиночные символы
+    for i in range(length):
+        yield text[i]  # Возвращаем одиночный символ
+    # Генерируем подпоследовательности длиной больше 1
+    for sub_length in range(2, length + 1):  # Длина подстроки от 2 до length
+        for start in range(length - sub_length + 1):  # Начальная позиция от 0 до length - sub_length
+            end = start + sub_length  # Конечная позиция
+            yield text[start:end]  # Возвращаем подстроку от start до end
 
 a = all_variants("abc")
 for i in a:
